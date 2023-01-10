7ecec15961540b7817f904122c10cb4b83a9e879
Donnie <donniezfr@foxmail.com>
2023-01-10 15:20:02 +0800

    Changes

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,90 @@
+package deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] items;
+    private int _size;
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        _size = 0;
+    }
+    private void insert(T item, int position){
+        T[] newItems = (T[]) new Object[items.length];
+        System.arraycopy(items,0,newItems,0,position);
+        newItems[position]=item;
+
+        System.arraycopy(items, position, newItems, position+1, items.length-position);
+        items = newItems;
+    }
+    private void resize(int capacity){
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, _size);
+        items = a;
+    }
+    @Override
+    public void addFirst(T item) {
+        insert(item,0);
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (_size == items.length) {
+            resize(_size + 1);
+        }
+
+        items[_size] = item;
+        _size = _size + 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return _size == 0;
+    }
+
+    @Override
+    public int size() {
+        return _size;
+    }
+
+    @Override
+    public void printDeque() {
+        for(T i:items){
+            System.out.print(i+" ");
+            System.out.println();
+        }
+    }
+    private void decreaseResize(){
+        if(items.length/_size>4){
+            resize(_size*2);
+        }
+    }
+    @Override
+    public T removeFirst() {
+        T[] newItems = (T[]) new Object[items.length-1];
+        System.arraycopy(items,1,newItems,0,items.length-1);
+        T removed = items[0];
+        items = newItems;
+        _size--;
+        decreaseResize();
+        return removed;
+    }
+
+    @Override
+    public T removeLast() {
+        T removed = items[items.length-1];
+        _size--;
+        decreaseResize();
+        return removed;
+    }
+
+    @Override
+    public T get(int index) {
+        return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,96 @@
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> implements Deque<T> {
+
+    private class IntNode{
+        public IntNode prev;
+        public IntNode next;
+        public T item;
+        public IntNode(T i, int x, IntNode n){
+            item = i;
+            if(x==1)next = n;
+            else if(x==0)prev=n;
+        }
+    }
+    private IntNode sentinel ;
+    private int _size;
+    public LinkedListDeque(){
+        sentinel=new IntNode(null,0,null);
+        sentinel.prev=sentinel;
+        sentinel.next=sentinel;
+        _size = 0;
+    }
+    @Override
+    public void addFirst(T item) {
+        _size+=1;
+        sentinel.next = new IntNode(item,1,sentinel.next);
+        sentinel.next.prev=sentinel;
+        sentinel.next.next.prev=sentinel.next;
+    }
+
+    @Override
+    public void addLast(T item) {
+        _size+=1;
+        sentinel.prev= new IntNode(item,0,sentinel.prev);
+        sentinel.prev.next=sentinel;
+        sentinel.prev.prev.next=sentinel.prev;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return _size == 0;
+    }
+
+    @Override
+    public int size() {
+        return _size;
+    }
+
+    @Override
+    public void printDeque() {
+        IntNode p=sentinel;
+        while(p.next!=sentinel){
+            System.out.print(p.next.item+" ");
+            p=p.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if(_size == 0) return null;
+        _size-=1;
+        T L = sentinel.next.item;
+        sentinel.next.next.prev=sentinel;
+        sentinel.next=sentinel.next.next;
+        return L;
+//        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        if(_size == 0) return null;
+        _size-=1;
+        T L = sentinel.prev.item;
+        sentinel.prev.prev.next=sentinel;
+        sentinel.prev=sentinel.prev.prev;
+        return L;
+        //return null;
+    }
+
+    @Override
+    public T get(int index) {
+        IntNode p=sentinel;
+        for(int i=0;i<index;i++){
+            p=p.next;
+        }
+        return p.next.item;
+//        return null;
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
